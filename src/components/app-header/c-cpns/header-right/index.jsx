7f72db3d493c211da4e6711f67ba1e78f29aab7a
Etcetera <yilonglei790@gmail.com>
2022-09-30 11:47:21 +0800

    头部栏框架搭建完成

--- src/components/app-header/c-cpns/header-right/index.jsx
+++ src/components/app-header/c-cpns/header-right/index.jsx
@@ -1,10 +1,29 @@
 import IconAvatar from "@/assets/svg/icon_avatar"
 import IconGlobal from "@/assets/svg/icon_global"
 import IconMenu from "@/assets/svg/icon_menu"
-import React, { memo } from "react"
+import React, { memo, useEffect, useState } from "react"
 import { RightWrapper } from "./style"
 
 const HeaderRight = memo(() => {
+  const [showPanel, setShowPanel] = useState(false)
+
+  // 副作用代码
+  useEffect(() => {
+    function windowHandleClick() {
+      setShowPanel(false)
+    }
+
+    window.addEventListener("click", windowHandleClick, true)
+
+    return () => {
+      window.removeEventListener("click", windowHandleClick, true)
+    }
+  }, [])
+
+  function profileClickHandle() {
+    setShowPanel(true)
+  }
+
   return (
     <RightWrapper>
       <div className='btns'>
@@ -15,9 +34,26 @@ const HeaderRight = memo(() => {
         </span>
       </div>
 
-      <div className='profile'>
+      <div
+        className='profile'
+        onClick={profileClickHandle}
+      >
         <IconMenu />
         <IconAvatar />
+
+        {showPanel && (
+          <div className='panel'>
+            <div className='top'>
+              <div className='item register'>注册</div>
+              <div className='item login'>登录</div>
+            </div>
+            <div className='bottom'>
+              <div className='item'>出租房源</div>
+              <div className='item'>开展体验</div>
+              <div className='item'>帮助</div>
+            </div>
+          </div>
+        )}
       </div>
     </RightWrapper>
   )
