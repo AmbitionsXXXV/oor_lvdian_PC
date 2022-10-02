cd017167230e65eb1fc4f30ea6527cc9f9952981
Etcetera <yilonglei790@gmail.com>
2022-10-02 21:47:31 +0800

    房间详情

--- src/views/entire/c-cpns/entire-rooms/index.jsx
+++ src/views/entire/c-cpns/entire-rooms/index.jsx
@@ -1,16 +1,30 @@
 import RoomItem from "@/components/room-item"
-import React, { memo } from "react"
-import { shallowEqual, useSelector } from "react-redux"
+import { changeDetailInfoAction } from "@/store/modules/detail"
+import React, { memo, useCallback } from "react"
+import { shallowEqual, useDispatch, useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
 import { RoomsWrapper } from "./style"
 
 const EntireRooms = memo(() => {
-  // 从redux中获取roomList数据
-  const { roomList, totalCount } = useSelector(
+  /** 从redux中获取roomList数据 */
+  const { roomList, totalCount, isLoading } = useSelector(
     (state) => ({
       roomList: state.entire.roomList,
       totalCount: state.entire.totalCount,
+      isLoading: state.entire.isLoading,
     }),
     shallowEqual
+  )
+
+  /** 事件处理 */
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const itemClickHandle = useCallback(
+    (item) => {
+      dispatch(changeDetailInfoAction(item))
+      navigate("/detail")
+    },
+    [navigate, dispatch]
   )
 
   return (
@@ -22,11 +36,14 @@ const EntireRooms = memo(() => {
             <RoomItem
               itemData={item}
               itemWidth='20%'
-              key={item.id}
+              key={item._id}
+              itemClick={itemClickHandle}
             />
           )
         })}
       </div>
+
+      {isLoading && <div className='cover'></div>}
     </RoomsWrapper>
   )
 })
